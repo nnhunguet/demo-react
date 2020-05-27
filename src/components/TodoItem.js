f39f06259492ee7e33dcb95a11322d4e40954487
nnhunguet <52577080+nnhunguet@users.noreply.github.com>
2020-05-28 01:03:46 +0700

    Add icon

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,5 +1,8 @@
-import React, { Component } from 'react'
-import './TodoItem.css'
+import React, { Component } from 'react';
+import './TodoItem.css';
+import { ReactComponent as Check } from './icon/check.svg';
+import { ReactComponent as CheckComplete } from './icon/check-complete.svg'
+
 
 class TodoItem extends Component {
   render() {
@@ -7,7 +10,10 @@ class TodoItem extends Component {
     const { item, onClick } = this.props;
     let className = classNames('TodoItem', {'TodoItem-complete': item.isComplete});
     return (
-      <div className={className} onClick={onClick}>
+      <div className={className}>
+        {
+          item.isComplete ? <CheckComplete onClick={onClick}/> : <Check onClick={onClick}/>
+        }
         <p>{item.title}</p>
       </div>
     )
